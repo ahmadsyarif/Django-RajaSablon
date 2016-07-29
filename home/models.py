0eb2f40d5ca9af44ab3da02a098aff5a283a6fb1
ahmadsyarif <ahmadsyarif92@gmail.com>
2016-07-29 20:08:31 +0800

    create prototype model for product and category

--- home/models.py
+++ home/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+"""
+Defenition of class product
+"""
+class product(models.Model):
+    name = models.CharField()
+    price = models.IntegerField()
+    description = models.TextField()
+    discount = models.FloatField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_last_modified = models.DateTimeField(auto_now=True)
+    created_by = models.CharField()
+    #TODO to create image field and its callable function to upload image in external repository
+    #image = models.ImageField()
+    available = models.IntegerField(default=0)
+
+    #manager
+    products = models.Manager()
+
+    def __str__(self):
+        return '{}'.format(self.name)
+"""
+Defenition of class Category
+"""
+class category(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_last_modified = models.DateTimeField(auto_now=True)
+    created_by = models.CharField()
+
+    #many to many relationship with product
+    products = models.ManyToManyField(product)
+
+    #manager
+    categories = models.Manager()
