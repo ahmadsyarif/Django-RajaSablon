f184137a674679f8d6c6e33ea6a82ecf0d7b4d96
ahmadsyarif <ahmadsyarif92@gmail.com>
2016-07-29 20:35:28 +0800

    register product and category to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import product, category
 # Register your models here.
+
+#register product
+admin.site.register(product)
+
+#register category
+admin.site.register(category)
