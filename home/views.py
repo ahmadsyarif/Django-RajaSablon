3e9ddd989f909dcfc6c0d49440050725bb1c850b
ahmadsyarif <ahmadsyarif92@gmail.com>
2016-08-23 22:25:42 +0800

    show upload form

--- home/views.py
+++ home/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+from .form import *
 # Create your views here.
 
 def index(request):
     return HttpResponse("hello world, rita I love you")
 
 def upload(request):
+    form = productForm()
     context = {
-        'page':"upload"
+        'page':"upload",
+        'form':form
     }
     return render(request,'home/main.html',context)
